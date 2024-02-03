702fbc1915f17778ed90b8813f5a5adc22c2b6ec
takemi.ohama <takemi.ohama@gmail.com>
2024-02-03 09:04:10 +0900

    first commit

---
+++ bqkintone/__init__.py
@@ -0,0 +1,17 @@
+from .aurora import *
+from .awstool import *
+from .bigquery import *
+from .bq_kintone import *
+from .config_abc import *
+from .elasticcache import *
+from .etltool import *
+from .evaluator import *
+from .kintone import *
+from .parallelget import *
+
+__copyright__ = 'Copyright (C) 2023 Takemi Ohama'
+__VERSION__ = '0.0.0'
+__license__ = 'MIT'
+__author__ = 'Takemi Ohama'
+__author_email__ = 'takmei.ohama@gmail.com'
+__url__ = 'https://github.com/takemi-ohama/mllib'
